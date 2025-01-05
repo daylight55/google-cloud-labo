94e19df3bd9754b8e4518e5eccdcbcb32f00d918
daylight55 <jojomasanori3@gmail.com>
2025-01-05 23:12:32 +0900

    feature(cloud-dns): Add remove script for terraform destroy

--- terraform/cloud-dns/main.tf
+++ terraform/cloud-dns/main.tf
@@ -5,3 +5,24 @@ resource "google_dns_managed_zone" "main" {
 
   visibility = "public"
 }
+
+# NOTE: 一括削除用リソース
+resource "null_resource" "dns_record_cleanup" {
+  triggers = {
+    zone_name = google_dns_managed_zone.main.name
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<-EOT
+      records=$(gcloud dns record-sets list --zone=${self.triggers.zone_name} --format='get(name,type)' --filter='type!="NS" AND type!="SOA"')
+      while IFS= read -r record; do
+        if [ ! -z "$record" ]; then
+          name=$(echo $record | cut -d' ' -f1)
+          type=$(echo $record | cut -d' ' -f2)
+          gcloud dns record-sets delete "$name" --type="$type" --zone=${self.triggers.zone_name} --quiet
+        fi
+      done <<< "$records"
+    EOT
+  }
+}
